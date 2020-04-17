f8e509560a7db7ddc465440ec0a95801fd0b3faf
Jae-Son Song <jaesonsong@gmail.com>
2020-04-17 13:28:32 -0400

    Finish N+1 Buster

--- W5D5/NPlusOneBuster/app/models/route.rb
+++ W5D5/NPlusOneBuster/app/models/route.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: routes
+#
+#  id         :bigint           not null, primary key
+#  number     :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Route < ApplicationRecord
   has_many :buses,
     class_name: 'Bus',
@@ -19,7 +28,37 @@ class Route < ApplicationRecord
     all_drivers
   end
 
+  # # My broken version
+  # def better_drivers_query
+  #   # TODO: your code here
+  #   buses = self.buses.includes(:drivers)
+
+  #   hash = {}
+
+  #   buses.each do |bus|
+  #     hash[bus.id] = bus.drivers.to_a
+  #   end
+
+  #   hash
+  # end
+
+  # Working version
   def better_drivers_query
-    # TODO: your code here
+    buses = self.buses.includes(:drivers)
+
+    hash = {}
+
+    buses.each do |bus|
+      drivers = []
+
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+
+      hash[bus.id] = drivers
+    end
+
+    hash    
   end
+
 end
